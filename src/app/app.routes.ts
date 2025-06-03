3d09eb63552e1272077b9cdc7f3afe0391fa2790
Esihackman <mabelabenahackman602@gmail.com>
2025-06-03 19:05:56 +0000

    fixed an error in the app.config file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: "Home",
+    component: InvoiceListComponent
+  }
+];
