420ec83328039404290d6eb4ee63c9fd7d505dc3
Esihackman <mabelabenahackman602@gmail.com>
2025-06-04 16:16:58 +0000

    created and styled the the invoice forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
-
+import { InvoiceFormsComponent } from './invoice-forms/invoice-forms.component'
 export const routes: Routes = [
-  {
-    path: '',
-    title: "invoice-app",
-    component: InvoiceListComponent
-  }
+  { path: '', redirectTo: '/invoices', pathMatch: 'full' }, 
+  { path: 'invoices', component: InvoiceListComponent },   
+  { path: 'invoice/new', component: InvoiceFormsComponent }, 
+  { path: '**', redirectTo: '/invoices' } 
 ];
