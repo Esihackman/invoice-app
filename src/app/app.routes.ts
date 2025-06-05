0a826f43bff4727a6fb269636625acce61310017
Esihackman <mabelabenahackman602@gmail.com>
2025-06-05 08:24:39 +0000

    created the edit invoice form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { InvoiceFormsComponent } from './invoice-forms/invoice-forms.component'
+import { EditInvoiceFormComponent } from './edit-invoice-form/edit-invoice-form.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: '/invoices', pathMatch: 'full' }, 
   { path: 'invoices', component: InvoiceListComponent },   
   { path: 'invoice/new', component: InvoiceFormsComponent }, 
+  { path: 'invoice/edit/:id', component: EditInvoiceFormComponent},
   { path: '**', redirectTo: '/invoices' } 
 ];
