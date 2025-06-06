dc5b12422d679b139dc603e904bd1b46a0c87e4e
Esihackman <mabelabenahackman602@gmail.com>
2025-06-06 09:26:54 +0000

    created the invoice detail and styled it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { InvoiceFormsComponent } from './invoice-forms/invoice-forms.component'
 import { EditInvoiceFormComponent } from './edit-invoice-form/edit-invoice-form.component';
+import { InvoiceDetailComponent } from './invoice-detail/invoice-detail.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/invoices', pathMatch: 'full' }, 
-  { path: 'invoices', component: InvoiceListComponent },   
-  { path: 'invoice/new', component: InvoiceFormsComponent }, 
-  { path: 'invoice/edit/:id', component: EditInvoiceFormComponent},
-  { path: '**', redirectTo: '/invoices' } 
+  { path: '', redirectTo: '/invoices', pathMatch: 'full' },
+  { path: 'invoices', component: InvoiceListComponent },
+  { path: 'invoices/new', component: InvoiceFormsComponent },
+  { path: 'invoices/:id', component: InvoiceDetailComponent },
+  { path: 'invoices/:id/edit', component: EditInvoiceFormComponent },
+  { path: '**', redirectTo: '/invoices' }
 ];
---
+++ src/app/invoice-detail/invoice-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { InvoiceService } from '../services/invoice.service';
+import { Invoice } from '../models/invoice.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-invoice-detail',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './invoice-detail.component.html',
+  styleUrl: './invoice-detail.component.scss'
+})
+export class InvoiceDetailComponent implements OnInit {
+  invoice$!: Observable<Invoice | undefined>;
+  isDeleteModalOpen: boolean = false;
+  currentInvoiceId: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private invoiceService: InvoiceService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.invoice$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = params.get('id');
+        this.currentInvoiceId = id;
+        return this.invoiceService.getInvoiceById(id || '');
+      })
+    );
+  }
+
+  
+    }
+  
+
